f505902361b03f10128a203ffcf51524560abbe8
alfarezza <alfarezza08@gmail.com>
2025-08-28 09:39:18 +0700

    update index code

---
+++ back-end/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+
+app.listen(port, () => {
+    console.log("Listening on port 3000")
+});
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello Back-End!</h1>')
+});
